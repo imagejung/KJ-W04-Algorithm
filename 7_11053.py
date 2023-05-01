9527e6f516f30432f1d54f3d8d4ddb1d8ff9683a
imagejung <ysjung9312@gmail.com>
2023-05-01 11:27:33 +0900

    230501, 11053 가장 긴 증가하는 부분수열

--- 7_11053.py
+++ 7_11053.py
@@ -1,1 +1,17 @@
-# 가장 긴 증가하는 부분 수열
+# 가장 긴 증가하는 부분수열
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [0 for i in range(n)]
+# 자기자신보다 작은수중 '가장 큰 누적 수열길이 +1' 을 dp에 계속 저장
+for i in range(n):
+    # 본인 앞에 배열 모두 검사
+    for j in range(i):
+        if a[i] > a[j] and dp[j] > dp[i]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(max(dp))
